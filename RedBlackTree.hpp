11e3a87c4459bdc7d591ace49d200e5f5cdd4dbb
kent1db <kent1db@gmail.com>
2021-11-23 12:37:39 +0100

    insert fix fixed, work well

--- RedBlackTree.hpp
+++ RedBlackTree.hpp
@@ -2,23 +2,27 @@
 #define RBTREE_HPP
 #include <iostream>
 #include "LexicoComp.hpp"
-#define REDC		"\x1B[31m"
-#define BLACKC	"\x1B[30m"
-#define RESETC	"\x1B[0m"
 
-enum e_color {black, red};
-
+#define REDC        "\x1B[31m"
+#define BLACKC    "\x1B[30m"
+#define RESETC    "\x1B[0m"
+enum e_color
+{
+	black, red
+};
 namespace ft {
-	template <typename T>
-	class tree {
+	template<typename T>
+	class tree
+	{
 	public:
-		struct node {
+		struct node
+		{
 			T data;
 			node *left;
 			node *right;
 			node *parent;
 			e_color color;
-			bool	empty;
+			bool empty;
 		};
 	private:
 		node *root;
@@ -34,6 +38,7 @@ namespace ft {
 			elem->color = red;
 			return (elem);
 		}
+
 		node *createElement(T data) {
 			elem = new node;
 			elem->data = data;
@@ -44,8 +49,10 @@ namespace ft {
 			elem->color = red;
 			return (elem);
 		}
+
 		/// Constructor ///
 		tree() : root(createElement()) { root->color = black; }
+
 		tree(T data) : root(createElement(data)) { root->color = black; }
 
 		/// Destructor ///
@@ -57,148 +64,127 @@ namespace ft {
 		node *getRoot() {
 			return (root);
 		}
-		node *getParent(node *elem){
+
+		node *getParent(node *elem) {
 			return (elem->parent);
 		}
-		node *getLeft(node *elem){
+
+		node *getLeft(node *elem) {
 			return (elem->left);
 		}
-		node *getRight(node *elem){
+
+		node *getRight(node *elem) {
 			return (elem->right);
 		}
 
-		node *insertElem(node *elem, node *root) {
+		node *insertElem(node *elem, node *root, bool on) {
 			if (elem == NULL) {
 				return (NULL);
 			}
 			if (elem->data < root->data) {
 				if (root->left)
-					insertElem(elem, root->left);
+					insertElem(elem, root->left, on);
 				else {
 					elem->parent = root;
 					root->left = elem;
+					if (on)
+						insertFix(elem);
 				}
-			}
-			else if (elem->data == root->data)
+			} else if (elem->data == root->data)
 				return (NULL);
 			else {
 				if (root->right)
-					insertElem(elem, root->right);
+					insertElem(elem, root->right, on);
 				else {
 					elem->parent = root;
 					root->right = elem;
+					if (on)
+						insertFix(elem);
 				}
 			}
-			insertFix(elem);
 			return (elem);
 		}
 
-		void insertFix(node *k) {
-			node *u;
-			while (k->parent->color == red) {
-				if (k->parent == k->parent->parent->right) {
-					u = k->parent->parent->left;
-					if (u->color == red) {
-						u->color = black;
-						k->parent->color = black;
-						k->parent->parent->color = red;
-						k = k->parent->parent;
+		void insertFix(node *elem) {
+			if (elem == NULL || elem->parent == NULL)
+				return;
+			while (elem->parent->color == red) {
+				if (elem->parent == elem->parent->parent->left) {
+					if (elem->parent->parent->right && elem->parent->parent->right->color == red) {
+						elem->parent->parent->right->color = black;
+						elem->parent->color = black;
+						elem->parent->parent->color = red;
+						elem = elem->parent->parent;
 					} else {
-						if (k == k->parent->left) {
-							k = k->parent;
-							rotateRight(k);
+						if (elem == elem->parent->right) {
+							node *tmp = elem->parent;
+							rotateLeft(elem);
+							elem = tmp;
 						}
-						k->parent->color = black;
-						k->parent->parent->color = red;
-						rotateLeft(k->parent->parent);
+						elem->parent->color = black;
+						elem->parent->parent->color = red;
+						rotateRight(elem->parent);
 					}
 				} else {
-					u = k->parent->parent->right;
-
-					if (u->color == red) {
-						u->color = black;
-						k->parent->color = black;
-						k->parent->parent->color = red;
-						k = k->parent->parent;
+					if (elem->parent->parent->left && elem->parent->parent->left->color == red) {
+						elem->parent->parent->left->color = black;
+						elem->parent->color = black;
+						elem->parent->parent->color = red;
+						elem = elem->parent->parent;
 					} else {
-						if (k == k->parent->right) {
-							k = k->parent;
-							rotateLeft(k);
+						if (elem == elem->parent->left) {
+							node *tmp = elem->parent;
+							rotateRight(elem);
+							elem = tmp;
 						}
-						k->parent->color = black;
-						k->parent->parent->color = red;
-						rotateRight(k->parent->parent);
+						elem->parent->color = black;
+						elem->parent->parent->color = red;
+						rotateLeft(elem->parent);
 					}
 				}
-				if (k == root) {
+				if (elem == root)
 					break;
-				}
 			}
 			root->color = black;
-//			if (elem == NULL || elem->parent == NULL)
-//				return ;
-//			node *p = elem->parent;
-//			node *gp = p->parent;
-//			while (p->color == red) {
-//				if (p == gp->left) {
-//					if (gp->right->color == red) {
-//						gp->right->color = black;
-//						p->color = black;
-//						gp->color = red;
-//						elem = gp;
-//					} else {
-//						if (elem == p->right) {
-//							node *tmp = elem;
-//							elem = p;
-//							rotateLeft(tmp);
-//						}
-//						p->color = black;
-//						gp->color = red;
-//						rotateRight(p);
-//					}
-//				} else {
-//					if (gp->left->color == red) {
-//						gp->left->color = black;
-//						p->color = black;
-//						gp->color = red;
-//						elem = gp;
-//					} else {
-//						if (elem == p->left) {
-//							node *tmp = elem;
-//							elem = p;
-//							rotateRight(tmp);
-//						}
-//						p->color = black;
-//						gp->color = red;
-//						rotateLeft(p);
-//					}
-//				}
-//				if (elem == root)
-//					break;
-//			}
-//			root->color = black;
-
-			}
-
-		void displayTree(node *root, std::string indent, bool last) {
-				if (root != NULL) {
-					std::cout << indent;
-					if (last) {
-						std::cout << "R----";
-						indent += "   ";
-					} else {
-						std::cout << "L----";
-						indent += "|  ";
-					}
-
-					std::string sColor = root->color ? REDC : BLACKC;
-					std::cout << sColor << root->data << RESETC << std::endl;
-					displayTree(root->left, indent, false);
-					displayTree(root->right, indent, true);
-				}
 		}
 
-		void deleteTree(node *root){
+		void deleteElem(T data) {
+			node *x;
+			node *y;
+			node *toDel = root;
+			while (toDel != NULL) {
+				if (toDel->data == data)
+					break;
+				if (toDel->data > data)
+					toDel = toDel->left;
+				else
+					toDel = toDel->right;
+			}
+			if (toDel == NULL)
+				return;
+			e_color saveColor = toDel->color;
+			if (toDel->left == NULL)
+				x = toDel->right;
+		}
+
+		void displayTree(node *root, std::string indent, bool last) {
+			if (root != NULL) {
+				std::cout << indent;
+				if (last) {
+					std::cout << "R----";
+					indent += "   ";
+				} else {
+					std::cout << "L----";
+					indent += "|  ";
+				}
+				std::string sColor = root->color ? REDC : BLACKC;
+				std::cout << sColor << root->data << RESETC << std::endl;
+				displayTree(root->left, indent, false);
+				displayTree(root->right, indent, true);
+			}
+		}
+
+		void deleteTree(node *root) {
 			if (root == NULL)
 				return;
 			deleteTree(root->left);
@@ -206,79 +192,47 @@ namespace ft {
 			delete root;
 		}
 
-		void rotateLeft(node *x) {
-			node * y = x->right;
-			x->right = y->left;
-			if (y->left != NULL) {
-				y->left->parent = x;
+		void rotateLeft(node *elem) {
+			if (elem == NULL || elem->parent == NULL || elem->parent->left == elem)
+				return;
+			node *parentElem = elem->parent;
+			node *gparentElem = elem->parent->parent;
+			parentElem->right = elem->left;
+			if (elem->left) {
+				elem->left->parent = parentElem;
+				elem->left = NULL;
 			}
-			y->parent = x->parent;
-			if (x->parent == nullptr) {
-				this->root = y;
-			} else if (x == x->parent->left) {
-				x->parent->left = y;
-			} else {
-				x->parent->right = y;
-			}
-			y->left = x;
-			x->parent = y;
-//			if (elem == NULL || elem->parent == NULL || elem->parent->left == elem)
-//				return ;
-//			node *parentElem = elem->parent;
-//			node *gparentElem = elem->parent->parent;
-//			parentElem->right = elem->left;
-//			if (elem->left) {
-//				elem->left->parent = parentElem;
-//				elem->left = NULL;
-//			}
-//			elem->parent = gparentElem;
-//			if (gparentElem == NULL)
-//				root = elem;
-//			else if (gparentElem->left == parentElem)
-//				gparentElem->left = elem;
-//			else
-//				gparentElem->right = elem;
-//			elem->left = parentElem;
-//			parentElem->parent = elem;
+			elem->parent = gparentElem;
+			if (gparentElem == NULL)
+				root = elem;
+			else if (gparentElem->left == parentElem)
+				gparentElem->left = elem;
+			else
+				gparentElem->right = elem;
+			elem->left = parentElem;
+			parentElem->parent = elem;
 		}
 
-		void rotateRight(node *x) {
-			node * y = x->left;
-			x->left = y->right;
-			if (y->right != NULL) {
-				y->right->parent = x;
+		void rotateRight(node *elem) {
+			if (elem == NULL || elem->parent == NULL || elem->parent->right == elem)
+				return;
+			node *parentElem = elem->parent;
+			node *gparentElem = elem->parent->parent;
+			parentElem->left = elem->right;
+			if (elem->right) {
+				elem->right->parent = parentElem;
+				elem->right = NULL;
 			}
-			y->parent = x->parent;
-			if (x->parent == nullptr) {
-				this->root = y;
-			} else if (x == x->parent->right) {
-				x->parent->right = y;
-			} else {
-				x->parent->left = y;
-			}
-			y->right = x;
-			x->parent = y;
-//			if (elem == NULL || elem->parent == NULL || elem->parent->right == elem)
-//				return ;
-//			node *parentElem = elem->parent;
-//			node *gparentElem = elem->parent->parent;
-//			parentElem->left = elem->right;
-//			if (elem->right) {
-//				elem->right->parent = parentElem;
-//				elem->right = NULL;
-//			}
-//			elem->parent = gparentElem;
-//			if (gparentElem == NULL)
-//				root = elem;
-//			else if (gparentElem->left == parentElem)
-//				gparentElem->left = elem;
-//			else
-//				gparentElem->right = elem;
-//			elem->right = parentElem;
-//			parentElem->parent = elem;
+			elem->parent = gparentElem;
+			if (gparentElem == NULL)
+				root = elem;
+			else if (gparentElem->left == parentElem)
+				gparentElem->left = elem;
+			else
+				gparentElem->right = elem;
+			elem->right = parentElem;
+			parentElem->parent = elem;
 		}
-
 	};
 }
-
 #endif
--- main.cpp
+++ main.cpp
@@ -7,23 +7,24 @@
 
 int 	main(void){
 
-	ft::tree<int> it(33);
-	it.insertElem(it.createElement(13), it.getRoot());
-	it.insertElem(it.createElement(53), it.getRoot());
-	it.insertElem(it.createElement(11), it.getRoot());
-	it.insertElem(it.createElement(21), it.getRoot());
-	it.insertElem(it.createElement(41), it.getRoot());
-	it.insertElem(it.createElement(61), it.getRoot());
-	it.insertElem(it.createElement(15), it.getRoot());
-	it.insertElem(it.createElement(31), it.getRoot());
-	it.insertElem(it.createElement(20), it.getRoot());
-	it.insertElem(it.createElement(42), it.getRoot());
-//	it.insertElem(it.createElement(43), it.getRoot());
-//	it.insertElem(it.createElement(56), it.getRoot());
-//	it.insertElem(it.createElement(36), it.getRoot());
-//	it.insertElem(it.createElement(27), it.getRoot());
-//	it.insertElem(it.createElement(52), it.getRoot());
-//	it.insertElem(it.createElement(45), it.getRoot());
+	ft::tree<int> it(10);
+	it.insertElem(it.createElement(13), it.getRoot(), 1);
+	it.insertElem(it.createElement(20), it.getRoot(), 1);
+	it.insertElem(it.createElement(30), it.getRoot(), 1);
+	it.insertElem(it.createElement(53), it.getRoot(), 1);
+	it.insertElem(it.createElement(11), it.getRoot(), 1);
+	it.insertElem(it.createElement(21), it.getRoot(), 1);
+	it.insertElem(it.createElement(41), it.getRoot(), 1);
+	it.insertElem(it.createElement(61), it.getRoot(), 1);
+	it.insertElem(it.createElement(15), it.getRoot(), 1);
+	it.insertElem(it.createElement(31), it.getRoot(), 1);
+	it.insertElem(it.createElement(42), it.getRoot(), 1);
+	it.insertElem(it.createElement(43), it.getRoot(), 1);
+	it.insertElem(it.createElement(56), it.getRoot(), 1);
+	it.insertElem(it.createElement(36), it.getRoot(), 1);
+	it.insertElem(it.createElement(27), it.getRoot(), 1);
+	it.insertElem(it.createElement(52), it.getRoot(), 1);
+	it.insertElem(it.createElement(45), it.getRoot(), 1);
 	it.displayTree(it.getRoot(), "", true);
 	return (0);
 }
